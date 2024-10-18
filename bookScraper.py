156686ec8495bd444ce7f6c6b12469d561571a68
Ahmed Ibrahim <ahmed.mardi09@gmail.com>
2024-10-18 16:15:33 -0400

    Define get_book function to hold titlestore, price, avalaibility in an array

--- bookScraper.py
+++ bookScraper.py
@@ -10,3 +10,19 @@ import time
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
+base_url = "https://books.toscrape.com/"
+max_number = 3
+
+def get_book(url):
+    driver.get(url)
+    time.sleep(5)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    books = soup.find_all('article', class_='product_pod')
+    book_data = []
+    for book in books:
+        title = book.h3.a['title']
+        price = book.find('p', class_='price_color').text
+        availability = book.find('p', class_='instock availability').text.strip()
+        book_data.append([title, price, availability])
+    return book_data
+
